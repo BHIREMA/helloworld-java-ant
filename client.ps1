fdaa15c78b5495cdbedf96f5bfbc0f11a939d40c
BHIREMA <basavaraj.hiremath@ametek.com>
2020-07-31 15:42:09 +0530

    script

--- client.ps1
+++ client.ps1
@@ -1,6 +1,7 @@
-﻿$securePassword = ConvertTo-SecureString "3927@Deepa" -AsPlainText -force
-$credential = New-Object System.Management.Automation.PsCredential("AMETEK-AD\BHiremath",$securePassword)
-$sess = New-PSSession -Credential $cred -ComputerName 10.156.1.20
+﻿$username = “AMETEK-AD\BHiremath”
+$password = “3927@Deepa”
+$credentials = New-Object System.Management.Automation.PSCredential $username,$password
+$sess = New-PSSession -Credential $credentials -ComputerName 10.156.1.20
 Invoke-Command -Session $sess -ScriptBlock {C:\data\server.ps1}
 Exit-PSSession
 Remove-PSSession $sess
