2baa82bf76dffa9761c0fee9b0ceab73341c4c64
BHIREMA <basavaraj.hiremath@ametek.com>
2020-07-31 12:34:35 +0530

    ps script

---
+++ client.ps1
@@ -0,0 +1,7 @@
+﻿$securePassword = ConvertTo-SecureString "3927@Deepa" -AsPlainText -force
+$credential = New-Object System.Management.Automation.PsCredential("AMETEK-AD\BHiremath",$securePassword)
+$sess = New-PSSession -Credential $cred -ComputerName 10.156.1.20
+Enter-PSSession $sess
+Get-Location
+Exit-PSSession
+Remove-PSSession $sess
