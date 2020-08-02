0061f50c0c651da7ef030f525a3d19e6860e382b
BHIREMA <basavaraj.hiremath@ametek.com>
2020-08-02 19:18:14 +0530

    script

--- client.ps1
+++ client.ps1
@@ -1,6 +1,1 @@
-﻿$securePassword = ConvertTo-SecureString "3927@Deepa" -AsPlainText -force
-$credential = New-Object System.Management.Automation.PsCredential("AMETEK-AD\BHiremath",$securePassword)
-$sess = New-PSSession -Credential $credential -ComputerName 10.156.1.20
-Invoke-Command -Session $sess -ScriptBlock {C:\data\server.ps1 –PassThru}
-Exit-PSSession
-Remove-PSSession $sess
+﻿psexec \\10.156.1.20 -u AMETEK-AD\BHiremath -p 3927@Deepa -i -d 2 cmd /c java -jar C:\TestAutomation\Commonplatform\Cockpit_jar\Cockpit_jar\Cockpit.jar
